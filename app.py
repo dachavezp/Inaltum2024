0d705afd55760c4719ecd3cc4ce5e42fa347e63e
Arturo Chavez <dachavez@bu.edu>
2024-07-02 16:42:43 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -16,15 +16,15 @@ st.markdown(f'<div style="margin-top: 60px;"></div>', unsafe_allow_html=True)
 
 # Configurar el sidebar con inputs y un botón.
 with st.sidebar:
-    st.header("¿Quién está viviendo ya en el piso?")
-    inquilino1 = st.text_input("Inquilino 1 (Requerido)")
-    inquilino2 = st.text_input("Inquilino 2 (Opcional)")
-    inquilino3 = st.text_input("Inquilino 3 (Opcional)")
+    st.header("Who is alredy in the house?")
+    inquilino1 = st.text_input("Participant ID 1 (Required)")
+    inquilino2 = st.text_input("Participant ID 2 (Optional)")
+    inquilino3 = st.text_input("Participant ID 3 (Optional)")
     
-    if st.button('BUSCAR NUEVOS COMPAÑEROS'):
+    if st.button('Look for new mate'):
         # Verifica que al menos un inquilino haya sido introducido
         if not inquilino1:
-            st.error("Por favor, introduce al menos el código del Inquilino 1.")
+            st.error("Introduce i ID please.")
         else:
             # Obtener los identificadores de inquilinos utilizando la función
             id_inquilinos = obtener_id_inquilinos(inquilino1, inquilino2, inquilino3)
@@ -45,11 +45,11 @@ elif resultado is not None:
     cols = st.columns((1, 2))  # Divide el layout en 2 columnas
     
     with cols[0]:  # Esto hace que el gráfico y su título aparezcan en la primera columna
-        st.write("Nivel de compatibilidad de cada nuevo compañero:")
+        st.write("Compatibility level among participants:")
         fig_grafico = generar_grafico_compatibilidad(resultado[1])
         st.pyplot(fig_grafico)
     
     with cols[1]:  # Esto hace que la tabla y su título aparezcan en la segunda columna
-        st.write("Comparativa entre compañeros:")
+        st.write("Comparative between participants:")
         fig_tabla = generar_tabla_compatibilidad(resultado)
         st.plotly_chart(fig_tabla, use_container_width=True)
--- ayudantes.py
+++ ayudantes.py
@@ -6,25 +6,25 @@ import streamlit as st
 # FUNCIÓN PARA GENERAR EL GRÁFICO DE COMPATIBILIDAD
 def generar_grafico_compatibilidad(compatibilidad):
     fig, ax = plt.subplots(figsize=(5, 4))
-    compatibilidad = compatibilidad.abs()
-    sns.barplot(x=compatibilidad.index, y=compatibilidad.values, ax=ax, color='lightblue')
+    compatibilidad = compatibilidad.abs() + 1
+    sns.barplot(x=compatibilidad.index, y=compatibilidad.values, ax=ax, color='#c97f47')
     sns.despine()
-    ax.set_xlabel('Identificador de Inquilino', fontsize=10)
-    ax.set_ylabel('Similitud (%)', fontsize=10)
+    ax.set_xlabel('Participant ID', fontsize=10)
+    ax.set_ylabel('Similarity (%)', fontsize=10)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-    ax.set_yticklabels(['{:.1f}%'.format(y * 1) for y in ax.get_yticks()])
+    ax.set_yticklabels(['{:.1f}%'.format(y * 3) for y in ax.get_yticks()])
     for p in ax.patches:
-        ax.annotate(f'{p.get_height()*1:.1f}%', (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 5), textcoords='offset points')
+        ax.annotate(f'{p.get_height()*3:.1f}%', (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 5), textcoords='offset points')
     return fig
 
 # FUNCIÓN PARA GENERAR LA TABLA DE COMPAÑEROS
 def generar_tabla_compatibilidad(resultado):
     resultado_0_with_index = resultado[0].reset_index()
-    resultado_0_with_index.rename(columns={'index': 'ATRIBUTO'}, inplace=True)
+    resultado_0_with_index.rename(columns={'index': 'Survey Question'}, inplace=True)
     fig_table = go.Figure(data=[go.Table(
         columnwidth = [20] + [10] * (len(resultado_0_with_index.columns) - 1),
-        header=dict(values=list(resultado_0_with_index.columns), fill_color='paleturquoise', align='left'),
-        cells=dict(values=[resultado_0_with_index[col] for col in resultado_0_with_index.columns], fill_color='lavender', align='left')
+        header=dict(values=list(resultado_0_with_index.columns), fill_color='#001f60', align='left'),
+        cells=dict(values=[resultado_0_with_index[col] for col in resultado_0_with_index.columns], fill_color='#d9d9d9', align='left')
     )])
     fig_table.update_layout(width=700, height=320, margin=dict(l=0, r=0, t=0, b=0))
     return fig_table
